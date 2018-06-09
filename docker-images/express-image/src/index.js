8c72c6737057ab1ab05bb318b810e234a4f323b0
mathieu.jee <mathieu.jee@heig-vd.ch>
2018-06-09 12:19:46 +0200

    index.js modified

--- docker-images/express-image/src/index.js
+++ docker-images/express-image/src/index.js
@@ -5,7 +5,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res) {
-	res.send(generateStudents());
+	res.send(generateFakeCountry());
 });
 
 app.get('/test', function(req, res) {
@@ -17,32 +17,27 @@ app.listen(3000, function() {
 });
 
 
-function generateStudents() {
-	var numberOfStudents = chance.integer({
-		min: 0,
-		max: 10
-	});
-	console.log("number of students = " + numberOfStudents);
-	var students = [];
-	for(var i = 0; i < numberOfStudents; i++) {
-		var gender = chance.gender();
-		var birthYear = chance.year({
-			min: 1986,
-			max: 1996
-		});
-		students.push({
-			firstname: chance.first({
-				gender: gender	
-			}),
-			lastname: chance.last(),
-			gender: gender,
-			birthday: chance.birthday({
-				year: birthYear
-			})
+function generateFakeCountry() {
+	var numberOfCountries = chance.integer({ min: 0, max: 10 });
+	console.log("number of countries = " + numberOfCountries);
+	
+	var countries = [];
+	
+	for(var i = 0; i < numberOfCountries; i++) {
+		var country     = chance.country({ full: true });
+		var capitalCity = chance.city();
+		var currency    = chance.currency();
+		var population  = chance.integer({ min: 1, max: 7000000000 });
+		
+		countries.push({
+			country:     country,
+			capitalCity: capitalCity,
+			currency:    currency,
+			population:  population
 		});
 	};
-	console.log("students: ");
-	console.log(students);
-	return students;
+	
+	console.log("countries: ");
+	console.log(countries);
+	return countries;
 }
-
